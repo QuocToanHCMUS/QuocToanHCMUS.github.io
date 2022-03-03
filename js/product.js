279f79ceea4ec407d56b0cf0490aecc03027af73
QuocToanHCMUS <88534870+QuocToanHCMUS@users.noreply.github.com>
2022-03-03 16:31:29 +0700

    Add files via upload

---
+++ js/product.js
@@ -0,0 +1,41 @@
+let thumbnails = document.getElementsByClassName('thumbnail');
+let slider = document.getElementById('slider-pd');
+
+let buttonRight = document.getElementById('slide-right');
+let buttonLeft = document.getElementById('slide-left');
+
+buttonLeft.addEventListener('click', function(){
+    slider.scrollLeft -= 125;
+})
+
+buttonRight.addEventListener('click', function(){
+    slider.scrollLeft += 125;
+})
+
+const maxScrollLeft = slider.scrollWidth - slider.clientWidth;
+// alert(maxScrollLeft);
+// alert("Left Scroll:" + slider.scrollLeft);
+
+//AUTO PLAY THE SLIDER 
+function autoPlay() {
+    if (slider.scrollLeft > (maxScrollLeft - 1)) {
+        slider.scrollLeft -= maxScrollLeft;
+    } else {
+        slider.scrollLeft += 1;
+    }
+}
+let play = setInterval(autoPlay, 10);
+
+// PAUSE THE SLIDE ON HOVER
+for (var i=0; i < thumbnails.length; i++){
+
+thumbnails[i].addEventListener('mouseover', function() {
+    clearInterval(play);
+});
+
+thumbnails[i].addEventListener('mouseout', function() {
+    return play = setInterval(autoPlay, 10);
+});
+}
+
+// thanks for watching if you like the video then subscribe my webcodecamp channel
